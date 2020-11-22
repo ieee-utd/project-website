203d96ee54108259525ab999c7aa5340f0637039
Aarifa Gowani <aarifagowani@hotmail.com>
2020-11-22 17:58:49 -0600

    add navbar

---
+++ src/elements/navbar.svelte
@@ -0,0 +1,8 @@
+<script>
+    import PageAboutUs from "./pages/PageAboutUs.svelte";
+    import PageOurTeam from "./pages/PageOurTeam.svelte";
+    import PagePrograms from "./pages/PagePrograms.svelte";
+    import PageTutoring from "./pages/PageTutoring.svelte";
+    import PageHKN from "./pages/PageHKN.svelte";
+    import PageContact from "./pages/PageContact.svelte";
+</script>
