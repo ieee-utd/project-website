a9cd8c84ab0630ee1528cba1ce8a5044e66d5ad9
bhabishya-shrestha <bhabisyas@gmail.com>
2021-04-01 21:42:52 -0500

    update-programs

--- src/components/Navbar/bigNavBar.svelte
+++ src/components/Navbar/bigNavBar.svelte
@@ -1,5 +1,8 @@
 <script>
     import {routeIsActive} from "svelte-router-spa";
+    let list = ["#Technical","#Forge","#Events","#Robotics","#IEEE"]
+    import * as animateScroll from "svelte-scrollto";
+
 </script>
 
 <div class="nav-center">
@@ -8,7 +11,19 @@
         <li><a href= "/" class="nav-link" class:active={routeIsActive('/')}>Home</a></li>
         <li><a href= "/aboutus" class="nav-link "class:active={routeIsActive('/aboutus')}>About us</a></li>
         <li><a href= "/ourTeam" class="nav-link" class:active={routeIsActive('/ourTeam')}>Our Team</a></li>
-        <li><a href= "/programs" class="nav-link" class:active={routeIsActive('/programs')}>Programs</a></li>
+        <div class = "dropdown">
+            <li><a href= "/programs" class="nav-link" class:active={routeIsActive('/programs')}>Programs</a></li>
+            <div class="dropdown-content">
+                {#each list as item}
+                        <a href = "/programs" on:click|preventDefault={() => animateScroll.scrollTo({element:item})}><a href = "/programs" on:click|preventDefault={() => animateScroll.scrollTo({element:item})}>{item.slice(1)}<br></a></a>
+                {/each}
+                <!-- <li><a href= "/programs" on:click|preventDefault={() => (y = 468)}>Technical</a></li>
+                <li><a href= "/programs" on:click|preventDefault={() => (y = 790)}>Forge</a></li>
+                <li><a href= "/programs" on:click|preventDefault={() => (y = 1100)}>Events</a></li>
+                <li><a href= "/programs" on:click|preventDefault={() => (y = 1)}>Robotics</a></li>
+                <li><a href= "/programs" on:click|preventDefault={() => (y = 1)}>IEEE</a></li> -->
+                </div>
+        </div>
         <li><a href= "/tutoring" class="nav-link" class:active={routeIsActive('/tutoring')}>Tutoring</a></li>
         <li><a href= "/hkn" class="nav-link" class:active={routeIsActive('/hkn')}>HKN</a></li>
         <li><a href= "/contact"class="nav-link" class:active={routeIsActive('/contact')}>Contact</a></li>
@@ -40,4 +55,22 @@
     .nav-link:hover{
         color:var(--color-accent);
     }
+    .dropdown {
+        position: relative;
+        display: inline-block;
+    }
+    .dropdown-content {
+        display: none;
+        position: absolute;
+        background-color: gray;
+        min-width: 115px;
+        box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+        z-index: 1;
+        opacity: 0.5;
+        font-size: 15px;
+
+    }
+    .dropdown-content a:hover {color: white;}
+    
+    .dropdown:hover .dropdown-content {display: block;}
 </style>
--- src/pages/PagePrograms.svelte
+++ src/pages/PagePrograms.svelte
@@ -6,41 +6,248 @@
   let title = 'program';
   
   let list = ["#section-1","#section-2","#section-3","#section-4","#section-5","#section-6","#section-7"]
-  </script>
-<main >
+ 
+  import { fly } from 'svelte/transition';
+  import { fade } from 'svelte/transition';
+  let y;
+ </script>
+<svelte:window bind:scrollY={y}/>
 
-<NavigationBtn {list} />
-<Banner {picDir} {title} id = "top"/>
-  <section id="section-1">
-    <h1>Section</h1>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus culpa, quo, esse, fugiat ut quis expedita suscipit delectus iste blanditiis officiis aspernatur. Corporis quae maiores veniam. Quae error sapiente inventore omnis, aut laborum fuga voluptas a mollitia provident alias eveniet. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus nesciunt, accusamus itaque quisquam quidem ex repellendus non obcaecati, expedita doloribus ipsa qui, minima laudantium voluptates aperiam amet minus architecto aliquid! Veniam cupiditate qui minus est nesciunt eum officiis maiores consequatur. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi quaerat fugiat numquam. Quis modi quas dolor, nisi quia molestiae? Eligendi neque voluptatem odit et esse amet numquam natus itaque beatae in, unde dolores pariatur dicta, sequi quae architecto maxime quaerat! Autem esse quasi ad animi saepe quod provident, quis dolorum mollitia perspiciatis fugiat repellat harum vero aspernatur. Quos non beatae odit qui? Vel quidem cumque quis nihil cum vero voluptatum eius optio unde, inventore est quo et quae excepturi atque iusto nemo corrupti dolores maiores dolorem earum aliquam </p>
-  </section>
-  <section id="section-2" >
-    <h1>Section</h1>
-    <p>dus nesciunt ut harum veniam repudiandae dolores natus quasi labore, molestiae pariatur molestias facere quas voluptatibus corrupti vitae? Ipsa molestias officiis reprehenderit voluptatum a magni fugit, quos doloribus excepturi placeat voluptates nemo minus nostrum repellendus magnam possimus corrupti exercitationem quasi porro iure, asperiores expedita illo veritatis! Deleniti modi labore, nulla quo autem qui earum illum? Praesentium impedit quasi quibusdam alias cumque nisi dignissimos odit modi, soluta, iure hic illum optio amet assumenda consequatur officiis numquam error harum quis quos, nam nemo neque? Hic quas qui repellendus deserunt, laborum sint quos at nostrum quisquam ipsam inventore neque. Aut vitae accusamus deleniti quaerat pariatur nobis ullam, quos sint omnis officia provident architecto eveniet. Placeat quos accusamus dolores laudantium.</p>
-  </section>
-  <section id="section-3" >
-    <h1>Section</h1>
-    <p>erferendis neque deserunt corporis, maxime doloribus ea illum doloremque facilis necessitatibus quod rem odit quae ducimus? Illo consequatur in dignissimos velit. Nobis maxime facere sed vel ea ipsa, doloremque deleniti neque, quam aliquam unde odit ullam libero nisi quod quos aperiam perspiciatis, expedita distinctio magnam reprehenderit inventore corrupti harum. Deleniti, et dolore magnam expedita aliquid accusamus quae incidunt, corporis veritatis molestias error? Quos sit et aut quisquam sint qui unde ipsum necessitatibus tenetur, culpa quis perspiciatis deleniti cupiditate itaque ab at tempore voluptate possimus aliquid accusamus. Culpa minima sapiente quae! Voluptatibus beatae eaque sapiente tempore, minima pariatur, vel debitis quo recusandae quos itaque molestias unde assumenda?</p>
-  </section>
-  <section id="section-4">
-    <h1>Section</h1>
-    <p>ditiis dignissimos eligendi quos assumenda veniam iusto vel rerum, voluptatem sequi? Laborum, eius quae eveniet asperiores quis quasi illo vitae pariatur perferendis reiciendis exercitationem dignissimos suscipit et sunt. Quisquam eum ex tempora! Nesciunt voluptatum ad, quod fugit quia nulla repellat commodi? Magnam earum qui similique aliquam cupiditate deserunt quas atque numquam, dignissimos id recusandae alias ratione molestiae non blanditiis. Quas enim dolorum tempora repudiandae?</p>
-  </section>
-  <section id="section-5">
-    <h1>Section</h1>
-    <p> commodi ipsa numquam reiciendis dicta earum totam nisi, molestiae vero in repellat magni? Omnis, deserunt consequatur quos incidunt architecto eaque aliquam quod harum explicabo quibusdam exercitationem amet ut obcaecati, voluptate molestiae! Nesciunt perspiciatis esse exercitationem vitae autem porro minima ad iure veniam dicta doloremque ipsam cupiditate vero nam in, repellat quae quam odit magnam quibusdam harum. Non quisquam id iste totam quas possimus rem sunt sit vel tempora accusantium voluptas consequatur nam minima explicabo aliquam neque rerum laborum quia ipsa voluptate, voluptates quaerat! Autem debitis voluptatibus culpa quas labore officia sit maiores sunt. Numquam tempora distinctio maiores vel sed, voluptatem quisquam veniam error dolorem, laborum inventore accusamus. Est sit recusandae repellendus in laudantium voluptatibus, quos corporis rem debitis, alias adipisci rerum ducimus voluptate nostrum, itaque impedit odio fuga. Perferendis totam aliquam quasi hic asperiores. Illum, ad? Fugiat officia blanditiis aperiam sequi doloremque vero ab repellendus omnis beatae commodi. Recusandae voluptatem, esse enim possimus nihil, vero temporibus rem, inventore distinctio magnam atque. Nihil, perferendis aspernatur.</p>
-  </section>
-  <section id="section-6">
-    <h1>Section</h1>
-    <p>nemo! Iusto nam possimus praesentium sed dignissimos itaque at incidunt, accusantium quidem autem reiciendis rerum fuga asperiores culpa pariatur quibusdam, accusamus quaerat ut consequuntur error, facere debitis amet qui. Repellat autem eveniet molestias debitis dolorem ullam beatae facere dolore sunt obcaecati vitae veniam amet, dicta fuga quisquam neque praesentium minima tempore! Itaque ad eveniet natus nam odit sit magni vitae dolorum rerum repellendus illum, quas ex architecto repellat culpa rem voluptatem quos necessitatibus reiciendis qui. Voluptatum excepturi ducimus, esse debitis facere amet corrupti consequatur sunt libero eius reiciendis, numquam error sit tempora. Quidem, aliquam delectus. Impedit, veritatis? Illum expedita deleniti sapiente, sit soluta possimus alias exercitationem minus repudiandae illo. Laboriosam illum necessitatibus sed porro laudantium maiores asperiores repudiandae velit neque, unde, alias voluptatem autem. Magnam unde culpa debitis similique distinctio nihil modi nostrum molestiae excepturi accusantium? Minus cum amet mollitia. Modi?</p>
-  </section>
-
-  <section id="section-7">
-    <h1>Section</h1>
-    <p>pa quibusdam debitis facilis, molestias asperiores distinctio illo quod neque possimus dolorem veritatis sequi modi numquam quis nam quisquam incidunt corporis! Ipsam explicabo sapiente rem ducimus illum possimus! Error eum unde aperiam dolores quidem praesentium? Debitis quod non itaque expedita, ipsum dolores odit voluptatum nihil vitae cupiditate temporibus laboriosam incidunt quae, quibusdam laborum enim repellat impedit iure fugiat architecto, voluptatibus eum a. Cumque nesciunt, repudiandae sed dolorum quam non itaque quibusdam aspernatur doloremque iste vel consectetur ab dicta, quo voluptas consequuntur nobis harum cum dolores nostrum adipisci nemo enim dolor velit. Quos!</p>
-  </section>
-</main>    
 <style>
+  *{
+    margin:0; 
+    padding:0;
+    box-sizing: border-box;
+  }
+  main {
+    margin: 0;
+    align-items: center; 
+    width: 100vw;
+    background: #EEEEEE;
+    
+  }
+
+  .title{
+    font-size: 1.5rem;
+    font-weight: 800;
+    padding-bottom: 1%;
+  }
+
+
+  .container{
+    max-width: 100%;
+    margin: 0 auto;
+    font-size: 14px;
+    padding: 25px;
+    padding-left: 15%;
+    padding-right: 15%;
+  }
+
+
   
 </style>
+
+<main id = program>
+<NavigationBtn {list} />
+<Banner {picDir} {title} id = "top"/>
+
+<section  in:fade="{{ y: 200, duration: 750 }}" class="tech commitee section" id = "Technical">
+  <div class="container">
+    <div >
+      <h2 id="tech" class = "title" >Technical Committee</h2>
+    </div>
+    <div class="section-center" >
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+      occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+      mollit anim id est laborum.
+      <br>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+      occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+      mollit anim id est laborum.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+      occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+      mollit anim id est laborum.
+      <br>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+      occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+      mollit anim id est laborum.
+    </p>
+  </div>
+</section>
+
+<section in:fade="{{ y: 200, duration: 750 }}" class="forge section" id = "Forge">
+  <div class="container">
+    <h2 class = "title">The Forge</h2>
+    <div class="section-center">
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+      occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+      mollit anim id est laborum.
+      <br>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+      occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+      mollit anim id est laborum.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+      occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+      mollit anim id est laborum.
+      <br>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+      occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+      mollit anim id est laborum.
+    </p>
+  </div>
+</section>
+
+  <section in:fade="{{ y: 200, duration: 750 }}" class="events committee section" id = "Events">
+    <div class="container">
+    <h2 class = "title">Events Committee </h2>
+    <div class="section-center">
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+      occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+      mollit anim id est laborum.
+      <br>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+      occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+      mollit anim id est laborum.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+      occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+      mollit anim id est laborum.
+      <br>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+      occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+      mollit anim id est laborum.
+    </p>
+  </div>
+</section>
+
+
+    <section in:fade="{{ y: 200, duration: 750 }}" class="robotics section" id = "Robotics">
+      <div class="container">
+      <h2 class = "title">Robotics</h2>
+      <div class="section-center">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+      occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+      mollit anim id est laborum.
+      <br>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+      occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+      mollit anim id est laborum.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+      occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+      mollit anim id est laborum.
+      <br>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+      occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+      mollit anim id est laborum.
+      </p>
+    </div>
+  </section>
+    
+
+    <section in:fade="{{ y: 200, duration: 750 }}" class="ieee wie section" id = "IEEE">
+      <div class="container">
+      <h2 class = "title">IEEE WIE</h2>
+      <div class="section-center">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+      occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+      mollit anim id est laborum.
+      <br>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+      occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+      mollit anim id est laborum.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+      occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+      mollit anim id est laborum.
+      <br>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+      occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+      mollit anim id est laborum.
+      </p>
+    </div>
+  </section>
+
+</main>
