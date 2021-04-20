7364a90afbc0e0b1d4f1f3bd5af7a9aef9aa43dc
nguyen tran <nguyen.tran6698@gmail.com>
2021-04-20 10:13:40 -0500

    Fixing Grid Table

--- src/pages/PageHKN.svelte
+++ src/pages/PageHKN.svelte
@@ -1,82 +1,69 @@
 <script>
-    import Banner from "../components/Banner/banner.svelte";
-    let picDir = "../ui/img/forge-program.jpg";
-    let title = "Eta Kappa Nu Honor Society";
+  import Banner from "../components/Banner/banner.svelte";
+  let picDir = "../ui/img/forge-program.jpg";
+  let title = "Eta Kappa Nu Honor Society";
+  import Grid from "./Grid.svelte";
+  let grid = [6, 4];
+</script>
 
-    import Grid from './Grid.svelte';
-	  let grid = [6, 4];
-  </script>
-
-
-   <style>
+<style>
+  .section {
+    padding: 3rem 2rem;
+    margin-bottom: 2rem;
+    width: 100%;
+  }
+  @media screen and (min-width: 776px) {
     .section {
-      padding: 3rem 2rem;
-      margin-bottom: 2rem;
-      width: 100%;
+      padding: 3rem 4rem;
     }
-    @media screen and (min-width: 776px) {
-      .section {
-        padding: 3rem 4rem;
-      }
-    }
+  }
 
-    .title{
+  .title {
     font-size: 1.5rem;
     font-weight: 800;
     padding-bottom: 1%;
-    }
+  }
 
-    div {
-		display: flex;
-		width: 200px;
-		justify-content: space-between;
-	}
-	
-  	div span:first-child {
-		margin-right: 20px;
-	}
-	
- 	  input {
-		width: 100%
-	}
-  </style>
-  
-  <Banner {picDir} {title} id="top" />
-  <main class="section">
-    <section id="section-1">
-      <p>
-        Eta Kappa Nu (HKN) is the honor society of IEEE, promoting professional accomplishment, service, and development for students and 
-        professionals in electrical engineering, computer engineering, and other IEEE fields of interest. More information regarding the 
-        2019-2020 cutoffs are coming soon.
-      </p>
-      <br>
-    </section>
-    <section id="section-2">
-    <h2 id="tech" class = "title" >Requirements</h2>
-        <p>
-        Below are the requirements Fall 2018 ECS majors with the cumulative GPA (Summer 2018) at the appropriate 1/5th, 1/4th, and 1/3rd point for HKN.
-        </p>
-        <br>
-        <style>
-          main {
-            ext-align: center;
-		
-		max-width: 1340px;
-		margin: 0 auto;
-          }
-        </style>
-        <Grid {grid} />
-        <br>
-        <p>
-          In addition, applicants will need to have completed 15 hours of volunteer service in STEM-related fields.
-          Fill out this <a style ="text-decoration:underline;" href="https://docs.google.com/forms/d/e/1FAIpQLSdPjSg_lHb4_-3eGkTYglESZ-Ssg1WYCq7RHAVyVIMPiRm7aw/viewform">interest form</a> form to learn more about HKN.
-        </p>
-      </section>
+  div {
+    display: flex;
+    width: 200px;
+    justify-content: space-between;
+  }
 
-      
- 
-  </main>
-  
-  
-  
+  div span:first-child {
+    margin-right: 20px;
+  }
 
+  input {
+    width: 100%;
+  }
+</style>
+
+<Banner {picDir} {title} id="top" />
+<main class="section">
+  <section id="section-1">
+    <p>
+      Eta Kappa Nu (HKN) is the honor society of IEEE, promoting professional
+      accomplishment, service, and development for students and professionals in
+      electrical engineering, computer engineering, and other IEEE fields of
+      interest. More information regarding the 2019-2020 cutoffs are coming
+      soon.
+    </p>
+    <br />
+  </section>
+  <section id="section-2">
+    <h2 id="tech" class="title">Requirements</h2>
+    <p>
+      Below are the requirements Fall 2018 ECS majors with the cumulative GPA
+      (Summer 2018) at the appropriate 1/5th, 1/4th, and 1/3rd point for HKN.
+    </p>
+    <Grid />
+    <p>
+      In addition, applicants will need to have completed 15 hours of volunteer
+      service in STEM-related fields. Fill out this <a
+        style="text-decoration:underline;"
+        href="https://docs.google.com/forms/d/e/1FAIpQLSdPjSg_lHb4_-3eGkTYglESZ-Ssg1WYCq7RHAVyVIMPiRm7aw/viewform">interest
+        form</a> form to learn more about HKN.
+    </p>
+  </section>
+</main>
--- src/pages/grid.svelte
+++ src/pages/grid.svelte
@@ -1,94 +1,102 @@
 <script>
-	export let grid = [4, 4];
-	
-	$: col = `repeat(${grid[1]}, 1fr)`;
-	$: row = `repeat(${grid[0]}, 1fr)`;
-	$: is_active = Array(grid[0]).fill(0).map(_ => Array(grid[1]).fill(false));
-	
-	let start = [];
-	let end = [];
-	let hover_end = []
-	let clicked = false;
-	
-	let list = ["ECS Major", "SO @ 1/5th", "JR @ 1/4th", "SR @ 1/3rd"]
+  export let grid = [4, 4];
 
-	let requirements = ["BMEN", "3.881", "3.801", "3.529",
-	                    "CE", "3.69", "3.442", "3.442",
-						"CS", "3.845", "3.738", "3.553",
-						"EE", "3.75", "3.584", "3.484",
-						"MECH", "3.704", "3.484", "3.375",
-						"SE", "3.651", "3.625", "3.413"]
-	let count = -1;
+  $: col = `repeat(${grid[1]}, 1fr)`;
+  $: row = `50px , repeat(${grid[0] - 1}, 1fr)`;
 
-	function updateCount(){
-		count++;
-		return count;
-	}
+  let list = ["ECS Major", "SO @ 1/5th", "JR @ 1/4th", "SR @ 1/3rd"];
+
+  let requirements = [
+    "BMEN",
+    "3.881",
+    "3.801",
+    "3.529",
+    "CE",
+    "3.69",
+    "3.442",
+    "3.442",
+    "CS",
+    "3.845",
+    "3.738",
+    "3.553",
+    "EE",
+    "3.75",
+    "3.584",
+    "3.484",
+    "MECH",
+    "3.704",
+    "3.484",
+    "3.375",
+    "SE",
+    "3.651",
+    "3.625",
+    "3.413",
+  ];
+  let count = -1;
+
+  function updateCount() {
+    count++;
+    return count;
+  }
 </script>
 
-
-
-<div class="container" style="grid-template-rows: {row}; grid-template-columns: {col};">
-	
-	{#each {length: 1} as _, k (k)}
-	{#each {length: 4} as _, l (l)}
-	<div class=grid style="background: {k.fixed};">
-		  <!-- {#each titleList as title}
-			  <li> {title.name} </li>
-		  {/each} -->
-		  <divTitle>
-			  <strong> {list[l]} </strong>
-		  </divTitle>
-		  
-		  
-  </div>
-	  {/each}
-  {/each}
-	{#each {length: grid[0]} as _, i (i)}
-	  {#each {length: grid[1]} as _, j (j)}
-	  <div class=grid  style="background: {i.fixed};">
-			<p>{requirements[updateCount()]} </p>
-	</div>
-		{/each}
-	{/each}
-
-</div>
-
 <style>
-	.container {
-		display: grid;
-		border: 0.01px solid #999;
-		border-radius: 2px;
-		width: 1000px;
-		height: 240px;
-		grid-row-gap: 1px;
-		border-bottom: 0.1px solid #999;
-		
-	} 
-	/* .container-title {
-		display: grid;
-		border-top: 0.1px solid #999;
-		border-right: 0.1px solid #999;
-		border-left: 0.1px solid #999;
-		border-radius: 2px;
-		width: 1000px;
-		height: 50px;
-		grid-row-gap: 1px;
-		background: #fff;
-	}  */
+  .container {
+    display: grid;
+    border: 0.01px solid #999;
+    border-radius: 2px;
+    width: 100%;
+    grid-row-gap: 1px;
+    background: #fff;
+				margin: 1rem 0;
+				
+				
+  }
+		.grid{
+			height:33.5px;
+			display: grid;
+			align-content: center;
+			border-bottom: 0.05px solid rgb(230, 228, 228);
+			padding-left:25px;
+		}
+		.grid-header{
+				height:50px !important;
+		}
 
-	.container div {
-		padding: 0 10px;
-		background: #fff;
-	}	
-
-
-	divTitle {
-	   height: 50px;
-       white-space: normal;
-       word-break: break-word;
-       display: flex;
-       align-items: center;
-	   color: rgb(38, 156, 195);
+  /* CUSTOM TAG for div title */
+  divTitle {
+    height: 50px;
+    white-space: normal;
+    word-break: break-word;
+    display: flex;
+    align-items: center;
+    color: rgb(38, 156, 195);
+  }
+	@media screen and (max-width:637px){
+		.grid{
+			min-width:169.5px;
+		}
+		.container{
+			overflow-x: scroll;
+		}
 	}
 </style>
+
+<div
+  class="container"
+  style="grid-template-rows: {row}; grid-template-columns: {col};">
+  {#each { length: 1 } as _, k (k)}
+    {#each { length: 4 } as _, l (l)}
+      <div class="grid grid-header" style="background: {k.fixed};">
+        <divTitle><strong> {list[l]} </strong></divTitle>
+      </div>
+    {/each}
+  {/each}
+  {#each { length: grid[0] } as _, i (i)}
+    {#each { length: grid[1] } as _, j (j)}
+      <div class="grid" style="background: {i.fixed};">
+        <p>{requirements[updateCount()]}</p>
+      </div>
+    {/each}
+  {/each}
+</div>
