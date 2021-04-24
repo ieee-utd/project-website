1b5c29dcac701145dc2ab6988d2f556612919e1d
nguyen tran <nguyen.tran6698@gmail.com>
2021-04-24 11:55:59 -0500

    Remove icon for Carousel

--- src/components/Carousel.svelte
+++ src/components/Carousel.svelte
@@ -1,7 +1,7 @@
 <script>
  import Carousel from "@beyonk/svelte-carousel";
  export let images = [];
- // import {ChevronLeftIcon, ChevronRightIcon} from "svelte-feather-icons";
+ import {ChevronLeftIcon, ChevronRightIcon} from "svelte-feather-icons";
 </script>
 <Carousel perPage=1 autoplay=6000 duration=3000 draggable=false>
  <!-- <span class="control" slot ="left-control"><ChevronLeftIcon/></span> -->
