83d3cfa1542470a005538521e7590e8591ecddc3
Bhabishya Shrestha <bhabisyas@gmail.com>
2021-04-28 19:27:57 -0500

    update navbar dropdown and sliding gallery image

--- src/components/Navbar/bigNavBar.svelte
+++ src/components/Navbar/bigNavBar.svelte
@@ -60,8 +60,11 @@
   font-size:0.85rem;
   text-transform: uppercase;
   font-weight: 600 ;
-
-
+  color: var(--color-paper-grey);
+  transition: 1s ease-in linear;
+}
+.drop-down-link:hover {
+  color: var(--color-white);
 }
 .dropdown:hover .dropdown-content {display: block;}
 
@@ -105,7 +108,7 @@
       <div class="dropdown-content">
           {#each list as item}
           <div>
-              <button on:click={() => handleClick(item.replace(/\s+/g, ''))}> <strong class = "nav-link drop-down-link">{item.slice(1)}</strong></button> 
+              <button on:click={() => handleClick(item.replace(/\s+/g, ''))}> <strong class = "drop-down-link">{item.slice(1)}</strong></button> 
           </div>
 
           {/each}
