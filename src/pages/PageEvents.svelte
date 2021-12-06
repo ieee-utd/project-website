fee49eee61e806c40576013e5eaa9483e2b88cb6
arod2613 <axr180039@utdallas.edu>
2021-12-05 19:20:35 -0600

    updated Events page

--- src/pages/PageEvents.svelte
+++ src/pages/PageEvents.svelte
@@ -1,3 +1,191 @@
 <script>
+    import Banner from "../components/Banner/banner.svelte";
+    let title = "Events";
+    let picDir = "../ui/img/IEEE_Group.jpg";
+    const date = new Date();
+	
+	const today = {
+		dayNumber: date.getDate(),
+		month: date.getMonth(),
+		year: date.getFullYear(),
+	}
+	
+	const monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+	let monthIndex = date.getMonth();
+	// const currentMonth = date.toLocaleString('en-US', { month: 'long' })
+	$: month = monthNames[monthIndex];
+	
+	let year = date.getFullYear();
+	
+	$: firstDayIndex = new Date(year, monthIndex, 1).getDay();
+	// const currentDay = date.getDate();
+	$: numberOfDays = new Date(year, monthIndex+1, 0).getDate();
+	
+	$: calendarCellsQty = numberOfDays + firstDayIndex;
+	
+	const goToNextMonth = () => {
+		if (monthIndex >= 11) {
+			year += 1;
+			return monthIndex = 0;
+		}
+		return monthIndex += 1;
+	}
+	
+	const goToPrevMonth = () => {
+		if (monthIndex <= 0) {
+			year -= 1;
+			return monthIndex = 11;
+		}
+		return monthIndex -= 1;
+	}
+	
+	$: console.log(`${month}, ${today.dayNumber}, ${year}, FIRST DAY index is ${firstDayIndex}, MONTH index is ${monthIndex}, No. of days: ${numberOfDays}`)
 </script>
-<h1>Events placeholder</h1>
+
+
+<Banner {title} {picDir} />
+<main>
+  <h3>
+    Check out our upcoming events:
+  </h3>
+  <div class="month">
+		<ul>
+			<li class="prev" on:click={goToPrevMonth}>&#10094;</li>
+			<li class="next" on:click={goToNextMonth}>&#10095;</li>
+			<li>{month}<br>
+				<span style="font-size:18px">{year}</span>
+			</li>
+		</ul>
+	</div>
+
+	<ul class="weekdays">
+		<li>Su</li>
+		<li>Mo</li>
+		<li>Tu</li>
+		<li>We</li>
+		<li>Th</li>
+		<li>Fr</li>
+		<li>Sa</li>
+	</ul>
+
+	<ul class="days">
+		{#each Array(calendarCellsQty) as _, i}
+			{#if i < firstDayIndex || i >= numberOfDays+firstDayIndex  }
+				<li>&nbsp;</li>
+			{:else}
+				<li class:active={i === today.dayNumber+(firstDayIndex-1) &&
+													monthIndex === today.month &&
+													year === today.year}>
+					{(i - firstDayIndex) + 1}
+				</li>
+			{/if}
+		{/each}
+	</ul>
+
+</main>
+
+<style>
+
+@font-face {
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 400;
+  src: url(https://fonts.gstatic.com/s/roboto/v29/KFOmCnqEu92Fr1Mu4mxK.woff2) format('woff2');
+  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+}
+    h3 {
+        color: black;
+        font-family: 'Roboto';
+        font-size: 1.5em;
+		text-align: left;
+		position: relative;
+		left: 100px;
+    }
+
+  ul {list-style-type: none;}
+
+	/* Month header */
+	.month {
+		padding: 70px 25px;
+		width: 88%;
+		background: #006400;
+		text-align: center;
+		position: relative;
+		left: 100px;
+	}
+
+	/* Month list */
+	.month ul {
+		margin: 0;
+		padding: 0;
+	}
+
+	.month ul li {
+		color: white;
+		font-size: 20px;
+		text-transform: uppercase;
+		letter-spacing: 3px;
+	}
+
+	/* Previous button inside month header */
+	.month .prev {
+		float: left;
+		padding-top: 10px;
+		cursor: pointer;
+	}
+
+	/* Next button */
+	.month .next {
+		float: right;
+		padding-top: 10px;
+		cursor: pointer;
+	}
+
+	/* Weekdays (Mon-Sun) */
+	.weekdays {
+		margin: 0;
+		width: 88%;
+		position: relative;
+		left: 100px;
+		padding: 10px 0;
+		background-color:#ddd;
+	}
+
+	.weekdays li {
+		display: inline-block;
+		width: 14%;
+		color: #666;
+		position: relative;
+		left: 114px;
+	}
+
+	/* Days (1-31) */
+	.days {
+		padding: 10px 0;
+		background: #eee;
+		margin: 0;
+		position: relative;
+		left: 100px;
+
+	}
+
+	.days li {
+		list-style-type: none;
+		display: inline-block;
+		border: 1px solid black;
+		padding: 9px;
+		width: 12.6%;
+		text-align: center;
+		margin-bottom: 1px;
+		font-size: 1.2rem;
+		color: #777;
+		cursor: pointer;
+	}
+
+	/* Highlight the "current" day */
+	.active {
+		padding: 5px;
+		background: #006400;
+		color: white !important
+	}
+  </style>
