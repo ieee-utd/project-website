f3df975d7ed7b982da20dd61308d2bc0eeea7262
Joseph Chang <joseph123749@gmail.com>
2021-12-05 19:26:29 -0600

    finish team page

--- src/pages/PageOurTeam.svelte
+++ src/pages/PageOurTeam.svelte
@@ -6,47 +6,73 @@
     let title = "our team";
 
 </script>
-  
+
+<head>
+  <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <style>
-   
-    .section {
-        margin-bottom: 2rem;
-        width: 100%;
-    }
-    .photo {
-      display: grid;
-      grid-template-areas:
-          "header header"
-          "sidebar content"
-          "footer footer";
-      grid-template-columns: 1fr 1fr 1fr 1fr;
-      grid-gap: 20px;
-    }
-    h1{
-      display: grid;
-      grid-template-columns: 1fr 1fr 1fr 1fr;
-      align-items: center;
-      grid-column-gap: 20px;
+  html {
+    box-sizing: border-box;
+  }
+  
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  
+  .column {
+    float: left;
+    width: 33.3%;
+    margin-bottom: 16px;
+    padding: 0 2rem;
+  }
+  
+  img {
+  object-fit: cover;
+  max-height:30rem;
+}
+
+  @media screen and (max-width: 650px) {
+    .column {
       
+      width: 100%;
+      display: block;
     }
-    .content {
-      margin-top: 0.5rem;
-    }
+  }
   
-    @media screen and (min-width: 776px) {
-  
+  .card {
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  }
+
+  .heading{
+      font-weight: bold;
+      font-size: 2rem;
+      padding-left: 2rem;
+      padding-bottom: 1rem;
+      margin-bottom: 2rem;
+      text-transform: uppercase;
     }
-    @media screen and (min-width: 992px) {
-      .section-center {
-        display: grid;
-        grid-template-columns: 1fr 1fr 1fr 1fr;
-        align-items: center;
-        justify-content: center;
-        grid-column-gap: 40px;
-      }
+
+  .container {
+    padding: 0 1rem;
+  }
   
-    }
+  .container::after, .row::after {
+    padding-left: 4.5rem;
+    content: "";
+    clear: both;
+    display: table;
+  }
+
+  .title {
+    color: grey;
+    font-family: 'Roboto', sans-serif;
+    text-transform: capitalize;
+    font-size: 1rem;
+  }
+  
   </style>
+</head>
+  
   
   <main id="our team">
     <Banner {picDir} {title}  id="top" />
@@ -65,73 +91,155 @@
             learn and develop connections with other engineers within their fields
             of interest
           </p>
-        </div>  
+        </div>
       </div>
     </section>
 
 
     <!-- team -->
-    <section class="team section" id="team">
-        <h class="title">{title}</h>        
-        <div class="photo">
-          <img src="../ui/img/default picture.jpg" class="team-photo" alt="Dylan" />
-          <img src="../ui/img/default picture.jpg" class="team-photo" alt="team" />
-          <img src="../ui/img/default picture.jpg" class="team-photo" alt="team" />
-          <img src="../ui/img/default picture.jpg" class="team-photo" alt="team" />
-          <header>Dylan Hiu</header>
-          <nav>Saravan Narayanan</nav>
-          <main>Julie Smith</main>
-          <footer>Aarifa Gowani</footer>
+    
+    <body>
+      <div class="heading">{title}</div>      
+      
+      <div class="row">
+        <div class="column">
+          <div class="card">
+            <img src="../ui/img/Officer Photos/IMG_Dylan_master.jpg" alt="Dylan" style="width:100%">
+            <div class="container">
+              <h2>Dylan Hiu</h2>
+              <p class="title">President</p>
+              <p>example@example.com</p>
+            </div>
+          </div>
         </div>
-        <div class="photo">
-          <img src="../ui/img/default picture.jpg" class="team-photo" alt="Dylan" />
-          <img src="../ui/img/default picture.jpg" class="team-photo" alt="team" />
-          <img src="../ui/img/default picture.jpg" class="team-photo" alt="team" />
-          <img src="../ui/img/default picture.jpg" class="team-photo" alt="team" />
-          <header>Leon Khalyavin</header>
-          <nav>Hansvi Patel</nav>
-          <main>Marie Sofijczuk</main>
-          <footer>Rooshi Nagar</footer>
+      
+        <div class="column">
+          <div class="card">
+            <img src="../ui/img/Officer Photos/IMG_Saravan_master.jpg" alt="Saravan" style="width:100%">
+            <div class="container">
+              <h2>Saravan Narayanan</h2>
+              <p class="title">Vice President</p>
+              <p>example@example.com</p>
+            </div>
+          </div>
         </div>
-        <div class="photo">
-          <img src="../ui/img/default picture.jpg" class="team-photo" alt="Dylan" />
-          <img src="../ui/img/default picture.jpg" class="team-photo" alt="team" />
-          <img src="../ui/img/default picture.jpg" class="team-photo" alt="team" />
-          <img src="../ui/img/default picture.jpg" class="team-photo" alt="team" />
-          <header>Leon Khalyavin</header>
-          <nav>Hansvi Patel</nav>
-          <main>Marie Sofijczuk</main>
-          <footer>Rooshi Nagar</footer>
-        </div>
-        <div class="photo">
-          <img src="../ui/img/default picture.jpg" class="team-photo" alt="Dylan" />
-          <img src="../ui/img/default picture.jpg" class="team-photo" alt="team" />
-          <img src="../ui/img/default picture.jpg" class="team-photo" alt="team" />
-          <img src="../ui/img/default picture.jpg" class="team-photo" alt="team" />
-          <header>Omar Faris</header>
-          <nav>Jenna Evans</nav>
-          <main>Victor Lopez</main>
-          <footer>Neil Rathod</footer>
-        </div>
-        <div class="photo">
-          <img src="../ui/img/default picture.jpg" class="team-photo" alt="Dylan" />
-          <img src="../ui/img/default picture.jpg" class="team-photo" alt="team" />
-          <img src="../ui/img/default picture.jpg" class="team-photo" alt="team" />
-          <img src="../ui/img/default picture.jpg" class="team-photo" alt="team" />
-          <header>Nuha Siddiqui</header>
-          <nav>Julie Smith</nav>
-          <main>Maxine Cabrasawan</main>
-          <footer>Dr. Andrea Fumagalli</footer>
-        </div>
-        <div class="photo">
-          <img src="../ui/img/default picture.jpg" class="team-photo" alt="Dylan" />
-          <img src="../ui/img/default picture.jpg" class="team-photo" alt="team" />
-        </div>
-        <div class="photo">
-          <header>Jerry Alexander</header>
-          <nav>Dr. Nasser Kehtarnavaz</nav>
+        
+        <div class="column">
+          <div class="card">
+            <img src="../ui/img/Officer Photos/IMG_Julie_master.jpg" alt="Julie" style="width:100%">
+            <div class="container">
+              <h2>Julie Smith</h2>
+              <p class="title">Vice President</p>
+              <p>example@example.com</p>
+            </div>
+          </div>
         </div>
 
-    </section>
+        <div class="column">
+          <div class="card">
+            <img src="../ui/img/Officer Photos/Aarifa - Edited.jpg" alt="Aarifa" style="width:100%">
+            <div class="container">
+              <h2>Aarifa Gowani</h2>
+              <p class="title">Head of The Forge</p>
+              <p>example@example.com</p>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <img src="../ui/img/Officer Photos/IMG_Leon_master.jpg" alt="Leon" style="width:100%">
+            <div class="container">
+              <h2>Leon Khalyavin</h2>
+              <p class="title">Head of The Forge</p>
+              <p>example@example.com</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="column">
+          <div class="card">
+            <img src="../ui/img/Officer Photos/IMG_Hansvi_master.jpg" alt="Hansvi" style="width:100%">
+            <div class="container">
+              <h2>Hansvi Patel</h2>
+              <p class="title">Director of Tutoring</p>
+              <p>example@example.com</p>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <img src="../ui/img/Officer Photos/IMG_Marie_master.jpg" alt="Marie" style="width:100%">
+            <div class="container">
+              <h2>Marie Sofijczuk</h2>
+              <p class="title">Director of Tutoring</p>
+              <p>example@example.com</p>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <img src="../ui/img/Officer Photos/IMG_Rooshi.jpg" alt="Rooshi" style="width:100%">
+            <div class="container">
+              <h2>Rooshi Nagar</h2>
+              <p class="title">Industry/Organization Liaison</p>
+              <p>example@example.com</p>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <img src="../ui/img/Officer Photos/IMG_Omar_master.jpg" alt="Omar" style="width:100%">
+            <div class="container">
+              <h2>Omar Faris</h2>
+              <p class="title">Director of Public Relations</p>
+              <p>example@example.com</p>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <img src="../ui/img/Officer Photos/IMG_Jenna_master.jpg" alt="Jenna" style="width:100%">
+            <div class="container">
+              <h2>Jenna Evans</h2>
+              <p class="title">Director of Public Relations</p>
+              <p>example@example.com</p>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <img src="../ui/img/Officer Photos/IMG_Victor_master.jpg" alt="Victor" style="width:100%">
+            <div class="container">
+              <h2>Victor Lopez</h2>
+              <p class="title">Director of Technology</p>
+              <p>example@example.com</p>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <img src="../ui/img/Officer Photos/IMG_Neil_master.jpg" alt="Neil" style="width:100%">
+            <div class="container">
+              <h2>Neil Rathod</h2>
+              <p class="title">Director of Technology</p>
+              <p>example@example.com</p>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <img src="../ui/img/Officer Photos/IMG_Nuha_master.jpg" alt="Nuha" style="width:100%">
+            <div class="container">
+              <h2>Nuha Siddiqui</h2>
+              <p class="title">Secretary</p>
+              <p>example@example.com</p>
+            </div>
+          </div>
+        </div>
+        
+
+      </div>
+      
+      </body>
       
   </main>
